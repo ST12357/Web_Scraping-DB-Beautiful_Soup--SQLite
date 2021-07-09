f9dd9d833a0c24bccb7ca12febf63755dceddd03
Shahriar Tasnim <39912482+ST12357@users.noreply.github.com>
2021-07-09 15:26:56 +0600

    Add files via upload

---
+++ startechbd/startechbd/spiders/startech_laptops.py
@@ -0,0 +1,50 @@
+import scrapy
+
+
+class StartechLaptopsSpider(scrapy.Spider):
+    name = 'startech_laptops'
+    allowed_domains = ['https://www.startech.com.bd']
+    start_urls = ['https://www.startech.com.bd/laptop-notebook/laptop?sort=p.price&order=ASC&limit=90',
+                  'https://www.startech.com.bd/laptop-notebook/laptop?sort=p.price&order=ASC&limit=90&page=2',
+                  'https://www.startech.com.bd/laptop-notebook/laptop?sort=p.price&order=ASC&limit=90&page=3',
+                  'https://www.startech.com.bd/laptop-notebook/laptop?sort=p.price&order=ASC&limit=90&page=4',
+                  'https://www.startech.com.bd/laptop-notebook/laptop?sort=p.price&order=ASC&limit=90&page=5',
+                  'https://www.startech.com.bd/laptop-notebook/laptop?sort=p.price&order=ASC&limit=90&page=6',
+                  'https://www.startech.com.bd/desktops?sort=p.price&order=ASC&limit=90',
+                  'https://www.startech.com.bd/desktops?sort=p.price&order=ASC&limit=90&page=2',
+                  'https://www.startech.com.bd/desktops?sort=p.price&order=ASC&limit=90&page=3'
+                  ]
+
+    def parse(self, response):
+        print("procesing:"+response.url)
+
+        Product_Name = response.xpath(
+            '//h4[@class="p-item-name"]/a/text()').extract()
+            
+        Product_Categotry = response.xpath(
+            '//h6[@class="page-heading m-hide"]/text()').extract()*len(Product_Name)
+
+        Product_Description = [li.xpath('string(.)').extract(
+        )[0] for li in response.xpath('//div[@class="short-description"]/ul/li')]
+
+        Product_Price = response.xpath(
+            '//div[@class="p-item-price"]/span/text()').extract()
+
+        Product_Availability = response.xpath(
+            '//div[@class="actions"]/span/text()').extract()
+
+        Product_URL = response.xpath(
+            '//h4[@class="p-item-name"]/a/@href').extract()
+
+        row_data = zip(Product_Categotry, Product_Name, Product_Description,
+                       Product_Price, Product_Availability, Product_URL,)
+        for item in row_data:
+            scraped_info = {
+                'Page': response.url,
+                'Product_Category': item[0],
+                'Product_Name': item[1],
+                'Product_Description': item[2],
+                'Product_Price': item[3],
+                'Product_Availability': item[4],
+                'Product_URL': item[5]}
+            yield scraped_info
